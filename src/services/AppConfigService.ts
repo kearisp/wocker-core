368829d8075c65f609a5048a2e5429f6eba48567
Kris Papercut <krispcut@gmail.com>
2023-12-02 22:50:52 +0200

    v1.0.4

--- src/services/AppConfigService.ts
+++ src/services/AppConfigService.ts
@@ -11,6 +11,8 @@ abstract class AppConfigService {
     public abstract getPWD(): string;
     public abstract setPWD(pwd: string): void;
     public abstract getAppConfig(): Promise<AppConfig>;
+    public abstract getMeta(name: string, defaultValue?: string): Promise<string|undefined>;
+    public abstract setMeta(name: string, value: string | number | undefined): Promise<void>;
     public abstract getAllEnvVariables(): Promise<AppConfig["env"]>;
     public abstract getEnvVariable(name: string, defaultValue?: string): Promise<string|undefined>;
     public abstract setEnvVariable(name: string, value: string | number): Promise<void>;
--- src/types/AppConfig.ts
+++ src/types/AppConfig.ts
@@ -3,6 +3,7 @@ import {EnvConfig} from "./EnvConfig";
 
 export type AppConfig = {
     debug?: boolean;
+    meta: EnvConfig;
     env: EnvConfig;
     plugins: string[];
     projects: {
