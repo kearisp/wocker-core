4d9e98a2b7eba1f22b97de82dbfeb3171fc56eec
Kris Papercut <krispcut@gmail.com>
2024-03-26 22:31:37 +0200

    v1.0.6

--- src/decorators/Inject.ts
+++ src/decorators/Inject.ts
@@ -3,8 +3,6 @@ import {SELF_DECLARED_DEPS_METADATA} from "../env";
 
 export const Inject = <T = any>(token?: T): ParameterDecorator => {
     return (target, propertyKey, parameterIndex) => {
-        // console.log(">_<", token, target, propertyKey, parameterIndex);
-
         Reflect.defineMetadata(SELF_DECLARED_DEPS_METADATA, [
             ...Reflect.getMetadata(SELF_DECLARED_DEPS_METADATA, target) || [],
             {
