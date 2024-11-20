48cc986ef206f7ead171bcaa1d72aeb0d5ba066c
Kris Papercut <krispcut@gmail.com>
2024-11-20 12:20:25 +0200

    v1.0.21

--- src/utils/getProviderToken.ts
+++
@@ -1,6 +0,0 @@
-import {Provider} from "../types/Provider";
-
-
-export const getProviderToken = (provider: Provider) => {
-
-};
---
+++ src/utils/volumeParse.spec.ts
@@ -0,0 +1,29 @@
+import {describe, it, expect, beforeAll, afterEach} from "@jest/globals";
+import {Logger} from "@kearisp/cli";
+
+import {volumeParse} from "./volumeParse";
+
+
+describe("volumeParse", (): void => {
+    beforeAll((): void => {
+        Logger.mute();
+    });
+
+    afterEach((): void => {
+        Logger.debug("-".repeat(10));
+        Logger.mute();
+    });
+
+    it("Should be parsed", (): void => {
+        expect(volumeParse("")).toEqual({});
+        expect(volumeParse("./test:/test")).toEqual({
+            source: "./test",
+            destination: "/test"
+        });
+        expect(volumeParse("./test:/test:rw")).toEqual({
+            source: "./test",
+            destination: "/test",
+            options: "rw"
+        });
+    });
+});
