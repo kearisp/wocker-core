988bbd974a5ddb9e3845674db1fe2019a01062b5
Kris Papercut <krispcut@gmail.com>
2026-03-26 19:29:43 +0200

    v1.0.32

--- src/types/FileSystemDriver.ts
+++ src/types/FileSystemDriver.ts
@@ -2,29 +2,10 @@ import FS, {
     type Dirent,
     type EncodingOption,
     type BufferEncodingOption,
-    type Mode
+    type Mode,
+    type symlink
 } from "fs";
 
-
-type StreamOptions = {
-    flags?: string;
-    fd?: number;
-    mode?: number;
-    start?: number;
-    autoClose?: boolean;
-    emitClose?: boolean;
-    encoding?: BufferEncoding;
-    signal?: AbortSignal | null;
-    highWaterMark?: number;
-};
-
-export type WriteStreamOptions = StreamOptions & {
-    flush?: boolean;
-};
-
-export type ReadStreamOptions = StreamOptions & {
-    end?: number;
-};
 
 export interface FileSystemDriver {
     openSync(path: string, flags: FS.OpenMode, mode?: FS.Mode | null): number;
@@ -32,6 +13,7 @@ export interface FileSystemDriver {
     readSync(file: number, buffer: NodeJS.ArrayBufferView, offset: number, length: number, position: FS.ReadPosition | null): number;
     existsSync(path: string): boolean;
     statSync(path: string): FS.Stats;
+    lstatSync(path: string): FS.Stats;
     fstatSync(fd: number): FS.Stats;
     mkdirSync(path: string, options?: FS.MakeDirectoryOptions): void;
     readdir(path: string, options: any, callback: (err: NodeJS.ErrnoException | null, files: string[]) => void): void;
@@ -40,8 +22,8 @@ export interface FileSystemDriver {
     writeFileSync(path: string, data: string | Buffer | NodeJS.ArrayBufferView, options?: FS.WriteFileOptions): void;
     appendFileSync(path: FS.PathOrFileDescriptor, data: string | Uint8Array, options?: FS.WriteFileOptions): void;
     rmSync(path: string, options?: FS.RmOptions): void;
-    createWriteStream(path: string, options?: WriteStreamOptions): FS.WriteStream;
-    createReadStream(path: string, options?: ReadStreamOptions): FS.ReadStream;
+    createWriteStream(path: string, options?: FileSystemDriver.WriteStreamOptions): FS.WriteStream;
+    createReadStream(path: string, options?: FileSystemDriver.ReadStreamOptions): FS.ReadStream;
     watch(file: string, options: FS.WatchOptionsWithStringEncoding, listener?: FS.WatchListener<string | Buffer>): FS.FSWatcher;
     watchFile(path: string, listener: FS.StatsListener): FS.StatWatcher;
     cpSync(source: string, destination: string, opts?: FS.CopySyncOptions): void
@@ -49,4 +31,30 @@ export interface FileSystemDriver {
     readlinkSync(path: string, options?: EncodingOption | BufferEncodingOption): string | Buffer<ArrayBuffer>;
     chmodSync(path: string, mode: Mode): void;
     chownSync(path: string, uid: number, gid: number): void;
+    symlinkSync(target: string, path: string, type?: symlink.Type): void;
+    unlinkSync(path: string): void;
+}
+
+export namespace FileSystemDriver {
+    export type SymlinkType = symlink.Type;
+
+    export type StreamOptions = {
+        flags?: string;
+        fd?: number;
+        mode?: number;
+        start?: number;
+        autoClose?: boolean;
+        emitClose?: boolean;
+        encoding?: BufferEncoding;
+        signal?: AbortSignal | null;
+        highWaterMark?: number;
+    };
+
+    export type WriteStreamOptions = StreamOptions & {
+        flush?: boolean;
+    };
+
+    export type ReadStreamOptions = StreamOptions & {
+        end?: number;
+    };
 }
